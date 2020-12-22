930b15ea4fb421fd4b442c45fe65bb49d0a4a6cb
Finn Gaida <f@fga.pw>
2020-12-22 12:49:55 +0100

    remove models dir

--- src/lib/trains/base_trainer.py
+++ src/lib/trains/base_trainer.py
@@ -3,9 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import time
+
 import torch
-from progress.bar import Bar
 from models.data_parallel import DataParallel
+from progress.bar import Bar
 from utils.utils import AverageMeter
 
 
